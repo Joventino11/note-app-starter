e176aa31110e2436a176fba29687ca1015081850
Joventino_Grefalda <jovengrefalda111101@gmail.com>
2022-10-22 11:36:46 +0800

    add.js

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
-const write = require('./write');
-const cmd = process.argv[2];
+const add = require('./add')
+const read = require('./read')
+const write = require('./write')
+const cmd = process.argv
+const del = require('./del')
+const update = require('./update')
 
-//write(cmd);
+const data = process.argv
+//write(data)
 
-const read = require('./read');
+var note = {}
 
-console.log(read());
+if(data[2] == 'add') {
+   
+    note = {
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+   
+    var oldNote = read()
+   
+    add(note, oldNote);
+}
+if(data[2] == 'read') {
+    
+    const present = require('./present')
+
+    present(read())
+}
+if (data[2] === "delete") {
+    let id =data[3]
+    let oldNote = read()
+    let del = require('./del')
+    del(id, oldNote);
+
+    console.log(read());
+}
+if (cmd[2] == 'update') {
+    let note = {
+        id: data[3],
+        title: data[4],
+        body: data[5]
+
+    }
+    const oldNote = read ()
+    update(note, oldNote)
+    console.log(update);
+}
--- write.js
+++ write.js
@@ -1,6 +1,7 @@
 const fs = require ('fs');
 
-const write = function(data="Nothing"){
+const write = function(data="default"){
     fs.writeFileSync('note.txt', data);
 }
-module.exports = write;
+
+module.exports =write;
